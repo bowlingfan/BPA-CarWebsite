de8cd7a74f3081a03031b6b798ca3a8465bf4599
bowlingfan <57610654+bowlingfan@users.noreply.github.com>
2023-01-02 13:52:34 -0600

    1/2/2023 start

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,10 @@
-<template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
-</template>
+<script setup lang="ts">
+</script>
 
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
-</style>
+<template>
+  <main>
+    <div>
+      <h1>this is AboutView</h1>
+    </div>
+  </main>
+</template>
